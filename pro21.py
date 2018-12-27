f56b5ba203d6df34742b7d51f15726dd1dbd970d
vamshikrishna00 <40817293+vamshikrishna00@users.noreply.github.com>
2018-12-27 23:34:43 +0530

    Update pro21.py

--- pro21.py
+++ pro21.py
@@ -1,26 +1,26 @@
-a=int(input())
+z=int(input())
 count=0
 count1=0
 w=0
 e=0
-for i in range(a):
+for i in range(z):
     x=input().split()
     break
-if(a>=2 and a<100000):
-    if(a%2==0):
-        q=a//2
+if(z>=2 and z<100000):
+    if(z%2==0):
+        q=z//2
         for j in range(q):
             w=w+int(x[j])
             count+=1
-        for k in range(q,a):
+        for k in range(q,z):
             e=e+int(x[k])
             count1+=1
     else:
-        t=a//2+1
+        t=z//2+1
         for j in range(t):
             w=w+int(x[j])
             count+=1
-        for k in range(t,a):
+        for k in range(t,z):
             e=e+int(x[k])
             count1+=1
     if(w//count==e//count1):
